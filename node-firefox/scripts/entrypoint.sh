7144846ee533a14ec1330b5b47d97764937456a9
William H <sylus1984@gmail.com>
2017-11-24 11:36:43 -0500

    feat(lock): Remove lock file on start-up

--- node-firefox/scripts/entrypoint.sh
+++ node-firefox/scripts/entrypoint.sh
@@ -12,6 +12,7 @@ teardown()
 # http://blog.jeffterrace.com/2012/07/xvfb-memory-leak-workaround.html
 # https://gist.github.com/jterrace/2911875
 # https://github.com/LoicMahieu/alpine-wkhtmltopdf/blob/master/Dockerfile
+rm /tmp/.X99-lock
 export DISPLAY=":99.0"
 Xvfb $DISPLAY -screen 0 1280x1024x24 -ac -extension RANDR  &
 
